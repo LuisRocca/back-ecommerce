aa1b4e61cabf7fae53f6afec23906323deb41728
jose rangel <jl.rangel20@hotmail.com>
2022-01-25 15:55:37 -0500

    crear producto listo

--- api/src/Controllers/Admin/createProduct.js
+++ api/src/Controllers/Admin/createProduct.js
@@ -2,33 +2,57 @@ const { conn } = require("../../db");
 const { Product, Category, User } = conn.models;
 
 
-const createProduct = async (req, res)=>{
+const createProduct = async (req, res, next)=>{
     try{
         let {idUser, idCategory, name, description, image, color, price, stock, storage, connectivity, model, ram}=req.body;
 
-        let searchIdUser= await User.findAll({
-            where:{
-                id: idUser
-            }
-        });
+        if(idUser && idCategory){
 
-        if(!searchIdUser[0]){
-            return res.status(409).send("El ID del Usuario no es Admin");
-        }else{
-            let searchIdCategory=await Category.findAll({
+            let searchIdUser= await User.findAll({
                 where:{
-                    idCategory:idCategory
+                    id: idUser
                 }
             });
 
-            if(!searchIdCategory[0]){
-                return res.status(409).send("El ID de la Categoria no existe");
+            if(!searchIdUser[0]){
+                return res.status(409).send("El ID del Usuario no existe");
+            }else{
+                if(searchIdUser[0].admin===true){
+                    let searchIdCategory= await Category.findAll({
+                        where:{
+                            idCategory
+                        }
+                    });
+                    if(!searchIdCategory[0]){
+                        return res.status(409).send("El ID de la categoria no existe");
+                    }else{
+                        console.log("entra")
+                        let newProduct= await Product.create({
+                            name,
+                            description,
+                            image,
+                            color,
+                            price,
+                            stock,
+                            storage,
+                            connectivity,
+                            model,
+                            ram
+                        });
+                        newProduct.setCategory(idCategory)
+                        res.send(newProduct)
+                    }
+                }else{
+                    return res.status(408).send("el Usuario no es Adminstrador")
+                }
             }
-
+        }else{
+            return res.status(404).send("Ingrese idUser y idCategory")
         }
     }
     catch(err){
-        res.status(404).send("ha ocurrido un error en la db")
+        next(err)
+        //return res.status(404).send("ha ocurrido un error en la db")
     }
 }
 
