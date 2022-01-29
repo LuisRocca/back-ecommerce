eac5179bd775858748311b1b0d4b668a384cc219
Ariel Sotelo <arielsoda14@gmail.com>
2022-01-29 20:09:56 -0300

    inicio de tarea favoritos

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,12 +4,9 @@ const user = require('./user');
 const review = require('./review');
 const productApple = require('./products.js');
 const admin = require('./admin.js');
-<<<<<<< HEAD
 const category = require('./category');
 const mercadoPago = require('./mercadoPago');
-=======
 const order = require('./order.js')
->>>>>>> 4e692d0fb131f4d986c2f0a08a122cf0c61abd57
 
 
 const router = Router();
@@ -23,12 +20,9 @@ router.use('/review', review);
 router.use('/user', user);
 router.use('/products', productApple);
 router.use('/admin', admin);
-<<<<<<< HEAD
 router.use('/category', category);
 router.use('/mercadopago', mercadoPago);
-=======
 router.use('/order', order)
->>>>>>> 4e692d0fb131f4d986c2f0a08a122cf0c61abd57
 
 
 module.exports = router;
