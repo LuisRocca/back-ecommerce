7bedfe664842eea059643964f1386db9e4df3c0c
Tomas Farias <85124666+TomasFarias11@users.noreply.github.com>
2022-01-30 20:09:19 -0300

    Update Product.js

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -2,11 +2,11 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define('product', {
-        id: {
-            type: DataTypes.UUID,
-            primaryKey: true,
-            defaultValue: DataTypes.UUIDV4,
-        },
+//         id: {
+//             type: DataTypes.UUID,
+//             primaryKey: true,
+//             defaultValue: DataTypes.UUIDV4,
+//         },
         name: {
             type: DataTypes.STRING,
             allowNull: false,
