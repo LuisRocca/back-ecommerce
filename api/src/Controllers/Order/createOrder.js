6729cefa988d902d3b2c99695342f8c597e17c7a
Tomas Farias <farias.tomas@outlook.com>
2022-01-31 20:10:09 -0300

    probando

--- api/src/Controllers/Order/createOrder.js
+++ api/src/Controllers/Order/createOrder.js
@@ -7,12 +7,29 @@ const createOrder = async (req, res) => {
     const user = await User.findByPk(req.params.idUser)
     const order = await Order.findAll({where: {userId: req.params.idUser, status: 'open'}})
 
-        if (order.length > 0) {
-            const ordenModificar = await Order.findByPk(order[0].dataValues.id).then((order) => {
+    if (order.length > 0) {
+        const ordenModificar = await Order.findByPk(order[0].dataValues.id).then((order) => {
+        let carritoMod = []
+            if (order.carrito.length === 0) {
                 order.update({carrito: carrito})
-            })
-            res.status(200).json(ordenModificar)
-        } else {
+            // } else if (carrito.length === 0) {
+            //     order.update({carrito: carrito})
+            } else {
+                // const carritoMod = carrito.filter((e) => e.id !== order.carrito)
+                for (let i = 0; i < carrito.length; i++) {  // carrito uso cuando no estoy logueado
+                    for (let j = 0; j < order.carrito.length; j++) {
+                        if (carrito[i].id === order.carrito[j].id) {
+                            break;
+                        } else if (j === order.carrito.length - 1) {
+                            carritoMod.push(carrito[i])
+                        }
+                    }
+                }
+                order.update({carrito: [...order.carrito, ...carritoMod]})
+            }
+        })
+        res.status(200).json(ordenModificar)
+    } else {
             const orderCreated = await Order.create({
                     carrito: carrito,
                     status: status,
