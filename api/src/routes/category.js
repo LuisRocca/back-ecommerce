e4eac3cd21c9fd8fdc0425cc85fa67945b7c47ae
Ariel Sotelo <arielsoda14@gmail.com>
2022-02-01 17:33:13 -0300

    rutas admin/user

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -2,11 +2,12 @@ const { Router } = require('express');
 const { categoryAll } = require('../Controllers/Category/categoryAll');
 const { categoryCreate } = require('../Controllers/Category/categoryCreate');
 const { categoryDelete } = require('../Controllers/Category/categoryDelete');
-
+const { categoryId } = require('../Controllers/Category/categoryId');
 
 const router = Router();
 
 router.get('/', categoryAll);
+router.get('/id/:idCategory', categoryId);
 router.post('/create/', categoryCreate);
 router.delete('/delete/', categoryDelete);
 
