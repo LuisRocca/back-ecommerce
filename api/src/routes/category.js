6ba889439347ac6010c8742c5b67881dc4bb2d8a
jose rangel <jl.rangel20@hotmail.com>
2022-02-01 23:53:17 -0500

    agregando ruta para editar categoria

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const { categoryAll } = require('../Controllers/Category/categoryAll');
 const { categoryCreate } = require('../Controllers/Category/categoryCreate');
 const { categoryDelete } = require('../Controllers/Category/categoryDelete');
+const { editCategory } = require('../Controllers/Category/editCategory');
 
 
 const router = Router();
@@ -9,5 +10,6 @@ const router = Router();
 router.get('/', categoryAll);
 router.post('/create/', categoryCreate);
 router.delete('/delete/', categoryDelete);
+router.put('/edit/:id', editCategory);
 
 module.exports = router
