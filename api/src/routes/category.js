33fda57bc694b08fe04acfcb50c04db9b08ea5f2
jose rangel <jl.rangel20@hotmail.com>
2022-02-04 12:29:24 -0500

    ruta idcategory

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -9,7 +9,7 @@ const { categoryId } = require('../Controllers/Category/categoryId');
 const router = Router();
 
 router.get('/', categoryAll);
-router.get('/id/:idCategory', categoryId);
+router.get('/:id', categoryId);
 router.post('/create/', categoryCreate);
 router.delete('/delete/', categoryDelete);
 router.put('/edit/:id', editCategory);
